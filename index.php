eca5ecc4183505726ec8cb128d1243caf63a17d1
TaufeeqR <taurajap@gmail.com>
2018-11-15 12:34:33 +0200

    Added html and css to my quiz page.

--- index.php
+++ index.php
@@ -1,14 +1,127 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>General Knowledge Quiz</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>General Knowledge Quiz</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="../css/main.css" />
+        <script src="main.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="heading">
+                <h1 class="title">Are You Worthy?</h1>
+            </div>
+        </div>
+
+        <section>
+            <div class="question-1">
+                <h2>Q.1  What year did Albert Einstein die?</h2>
+                <div class="answers">
+                    <form action="">
+                        <input type="radio" name="answer" value="1954">1954
+                        <input type="radio" name="answer" value="1949">1949
+                        <input type="radio" name="answer" value="1960">1960
+                        <input type="radio" name="answer" value="1955">1955
+                    </form> 
+                </div>
+            </div>
+
+            <div class="question-2">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-3">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-4">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-5">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-6">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-7">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-8">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-9">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-10">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-11">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-12">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-13">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-14">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+            
+            <div class="question-15">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-16">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-17">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-18">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-19">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+
+            <div class="question-20">
+                <h2></h2>
+                <div class="answers"></div>
+            </div>
+        </section>
+    </body>
 </html>
