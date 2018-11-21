12471552579fb2670668c0c154d2af0dba520607
TaufeeqR <taurajap@gmail.com>
2018-11-21 09:28:19 +0200

    Updated Readme and quiz title.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Create-a-quiz
-Creating a quiz using PHP
+# Create-a-quiz - Lets get Quizical!!
+
+
+
+## Creating a quiz using PHP
+
+```
+In this repository, I have created a general knowledge quiz 
+using HTML for my content, CSS for styling and PHP for my logic.
+```
+
+## Software and Scripts used:
+
+``` 
+-Html
+-Css
+-Php
+-Vs code(Visual Studio Code)
+-Github
+```
+
+## Contributors
+
+```
+Taufeeq Rajap
+```
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <!-- The container for the heading below -->
         <div class="container">
             <div class="heading">
-                <h1 class="title">Are You Worthy?</h1>
+                <h1 class="title">Lets get Quizical!!</h1>
             </div>
         </div>
 <!-- THe heading container ends here -->
@@ -24,7 +24,7 @@
                     <div class="question-1">
                         <h2>Q.1  What year did Albert Einstein die?</h2>
                         <div class="answers">
-                            <input type="radio" name="q1-answers" id="1" value="1">1954
+                            <input type="radio" name="q1-answers" id="1" value="1" required>1954
                             <input type="radio" name="q1-answers" id="2" value="2">1949
                             <input type="radio" name="q1-answers" id="3" value="3">1960
                             <input type="radio" name="q1-answers" id="4" value="4">1955
@@ -36,7 +36,7 @@
                     <div class="question-2">
                         <h2>Q.2  Which is the largest planet in the solar system?</h2>
                         <div class="answers">
-                            <input type="radio" name="q2-answers" id="1" value="1">Jupiter
+                            <input type="radio" name="q2-answers" id="1" value="1" required>Jupiter
                             <input type="radio" name="q2-answers" id="2" value="2">Neptune
                             <input type="radio" name="q2-answers" id="3" value="3">Earth
                             <input type="radio" name="q2-answers" id="4" value="4">Mars                        
@@ -48,7 +48,7 @@
                     <div class="question-3">
                         <h2>Q.3  What colour is Cerulean?</h2>
                         <div class="answers">
-                            <input type="radio" name="q3-answers" id="1" value="1">Red
+                            <input type="radio" name="q3-answers" id="1" value="1" required>Red
                             <input type="radio" name="q3-answers" id="2" value="2">Blue
                             <input type="radio" name="q3-answers" id="3" value="3">Yellow
                             <input type="radio" name="q3-answers" id="4" value="4">Green
@@ -60,7 +60,7 @@
                     <div class="question-4">
                         <h2>Q.4  What are a group of Dolphins called?</h2>
                         <div class="answers">
-                            <input type="radio" name="q4-answers" id="1" value="1">School
+                            <input type="radio" name="q4-answers" id="1" value="1" required>School
                             <input type="radio" name="q4-answers" id="2" value="2">Herd
                             <input type="radio" name="q4-answers" id="3" value="3">Pod
                             <input type="radio" name="q4-answers" id="4" value="4">Pool
@@ -72,7 +72,7 @@
                     <div class="question-5">
                         <h2>Q.5 Who discovered Penicillin?</h2>
                         <div class="answers">
-                            <input type="radio" name="q5-answers" id="1" value="1">Alexander Fleming
+                            <input type="radio" name="q5-answers" id="1" value="1" required>Alexander Fleming
                             <input type="radio" name="q5-answers" id="2" value="2">Thomas Edison
                             <input type="radio" name="q5-answers" id="3" value="3">Marie Curie
                             <input type="radio" name="q5-answers" id="4" value="4">George Orwell
@@ -84,7 +84,7 @@
                     <div class="question-6">
                         <h2>Q.6 What date was President John F Kennedy assassinated?</h2>
                         <div class="answers">
-                            <input type="radio" name="q6-answers" id="1" value="1">November 22 1962
+                            <input type="radio" name="q6-answers" id="1" value="1" required>November 22 1962
                             <input type="radio" name="q6-answers" id="2" value="2">November 22 1963
                             <input type="radio" name="q6-answers" id="3" value="3">October 29 1964
                             <input type="radio" name="q6-answers" id="4" value="4">November 24 1962
@@ -96,7 +96,7 @@
                     <div class="question-7">
                         <h2>Q.7 How many men have walked on the moon?</h2>
                         <div class="answers">        
-                            <input type="radio" name="q7-answers" id="1" value="1">10
+                            <input type="radio" name="q7-answers" id="1" value="1" required>10
                             <input type="radio" name="q7-answers" id="2" value="2">5
                             <input type="radio" name="q7-answers" id="3" value="3">12
                             <input type="radio" name="q7-answers" id="4" value="4">9
@@ -108,7 +108,7 @@
                     <div class="question-8">
                         <h2>Q.8 Who has won the most Academy Awards?</h2>
                         <div class="answers">
-                            <input type="radio" name="q8-answers" id="1" value="1">James Cameron
+                            <input type="radio" name="q8-answers" id="1" value="1" required>James Cameron
                             <input type="radio" name="q8-answers" id="2" value="2">Walt Disney
                             <input type="radio" name="q8-answers" id="3" value="3">Katherine Hepburn
                             <input type="radio" name="q8-answers" id="4" value="4">Steven Spielberg
@@ -120,7 +120,7 @@
                     <div class="question-9">
                         <h2>Q.9 What is the currency of Brazil?</h2>
                         <div class="answers">
-                            <input type="radio" name="q9-answers" id="1" value="1">Dollar
+                            <input type="radio" name="q9-answers" id="1" value="1" required>Dollar
                             <input type="radio" name="q9-answers" id="2" value="2">The Bhat
                             <input type="radio" name="q9-answers" id="3" value="3">Real
                             <input type="radio" name="q9-answers" id="4" value="4">Krona
@@ -132,7 +132,7 @@
                     <div class="question-10">
                         <h2>Q.10 Name the three primary colours.</h2>
                         <div class="answers">          
-                            <input type="radio" name="q10-answers" id="1" value="1">Blue, green and blue
+                            <input type="radio" name="q10-answers" id="1" value="1" required>Blue, green and blue
                             <input type="radio" name="q10-answers" id="2" value="2">Red, yellow and blue
                             <input type="radio" name="q10-answers" id="3" value="3">Pink, purple and violet
                             <input type="radio" name="q10-answers" id="4" value="4">Blue, red and green
@@ -144,7 +144,7 @@
                     <div class="question-11">
                         <h2>Q.11 What is John Leach famous for making?</h2>
                         <div class="answers">                
-                            <input type="radio" name="q11-answers" id="1" value="1">Artist
+                            <input type="radio" name="q11-answers" id="1" value="1" required>Artist
                             <input type="radio" name="q11-answers" id="2" value="2">Racecar driver
                             <input type="radio" name="q11-answers" id="3" value="3">Skydiving
                             <input type="radio" name="q11-answers" id="4" value="4">Pottery
@@ -156,7 +156,7 @@
                     <div class="question-12">
                         <h2>Q.12 What is someone who shoes horses called?</h2>
                         <div class="answers">
-                            <input type="radio" name="q12-answers" id="1" value="1">A farrier
+                            <input type="radio" name="q12-answers" id="1" value="1" required>A farrier
                             <input type="radio" name="q12-answers" id="2" value="2">A blacksmith
                             <input type="radio" name="q12-answers" id="3" value="3">A builder
                             <input type="radio" name="q12-answers" id="4" value="4">A horseshoe maker
@@ -168,7 +168,7 @@
                     <div class="question-13">
                         <h2>Q.13 What is another word for lexicon?</h2>
                         <div class="answers">                 
-                            <input type="radio" name="q13-answers" id="1" value="1">Culloden
+                            <input type="radio" name="q13-answers" id="1" value="1" required>Culloden
                             <input type="radio" name="q13-answers" id="2" value="2">Glasgow
                             <input type="radio" name="q13-answers" id="3" value="3">Dictionary
                             <input type="radio" name="q13-answers" id="4" value="4">Cube
@@ -180,7 +180,7 @@
                     <div class="question-14">
                         <h2>Q.14 What is the diameter of Earth?</h2>
                         <div class="answers">
-                                <input type="radio" name="q14-answers" id="1" value="1">5000 km
+                                <input type="radio" name="q14-answers" id="1" value="1" required>5000 km
                                 <input type="radio" name="q14-answers" id="2" value="2">100,000 miles
                                 <input type="radio" name="q14-answers" id="3" value="3">8,000 miles
                                 <input type="radio" name="q14-answers" id="4" value="4">250,000 miles
@@ -192,7 +192,7 @@
                     <div class="question-15">
                         <h2>Q.15 What colour is a Himalayan poppy?</h2>
                         <div class="answers">                    
-                            <input type="radio" name="q15-answers" id="1" value="1">Blue
+                            <input type="radio" name="q15-answers" id="1" value="1" required>Blue
                             <input type="radio" name="q15-answers" id="2" value="2">Purple
                             <input type="radio" name="q15-answers" id="3" value="3">Salmon
                             <input type="radio" name="q15-answers" id="4" value="4">Violet
@@ -204,7 +204,7 @@
                     <div class="question-16">
                         <h2>Q.16 Which kind of bulbs were once exchanged as a form of currency?</h2>
                         <div class="answers">
-                            <input type="radio" name="q16-answers" id="1" value="1">Light bulbs
+                            <input type="radio" name="q16-answers" id="1" value="1" required>Light bulbs
                             <input type="radio" name="q16-answers" id="2" value="2">Tulips
                             <input type="radio" name="q16-answers" id="3" value="3">Halogen bulbs
                             <input type="radio" name="q16-answers" id="4" value="4">Compact fluorescent bulbs
@@ -216,7 +216,7 @@
                     <div class="question-17">
                         <h2>Q.17 During which century did Blackbeard become famous for seafaring?</h2>
                         <div class="answers">
-                            <input type="radio" name="q17-answers" id="1" value="1">18th century
+                            <input type="radio" name="q17-answers" id="1" value="1" required>18th century
                             <input type="radio" name="q17-answers" id="2" value="2">15th century
                             <input type="radio" name="q17-answers" id="3" value="3">7th century
                             <input type="radio" name="q17-answers" id="4" value="4">19th century
@@ -228,7 +228,7 @@
                     <div class="question-18">
                         <h2>Q.18 What sport is the Keirin event associated with?</h2>
                         <div class="answers">                 
-                            <input type="radio" name="q18-answers" id="1" value="1">Bowling
+                            <input type="radio" name="q18-answers" id="1" value="1" required>Bowling
                             <input type="radio" name="q18-answers" id="2" value="2">Cycling
                             <input type="radio" name="q18-answers" id="3" value="3">Figure Skating
                             <input type="radio" name="q18-answers" id="4" value="4">Dancing
@@ -240,7 +240,7 @@
                     <div class="question-19">
                         <h2>Q.19 What sports form an Olympic triathlon?</h2>
                         <div class="answers">
-                            <input type="radio" name="q19-answers" id="1" value="1">Weightlifting, wrestling and boxing
+                            <input type="radio" name="q19-answers" id="1" value="1" required>Weightlifting, wrestling and boxing
                             <input type="radio" name="q19-answers" id="2" value="2">Tennis, shooting and hockey
                             <input type="radio" name="q19-answers" id="3" value="3">Javelin, long jump and high jump
                             <input type="radio" name="q19-answers" id="4" value="4">Swimming, cycling and running
@@ -252,7 +252,7 @@
                     <div class="question-20">
                         <h2>Q.20 What is a cosmonaut?</h2>
                         <div class="answers">
-                            <input type="radio" name="q20-answers" id="1" value="1">A Russian space traveller
+                            <input type="radio" name="q20-answers" id="1" value="1" required>A Russian space traveller
                             <input type="radio" name="q20-answers" id="2" value="2">A German space traveller
                             <input type="radio" name="q20-answers" id="3" value="3">A cosmopolitan magazine that went to space
                             <input type="radio" name="q20-answers" id="4" value="4">A South African space traveller
